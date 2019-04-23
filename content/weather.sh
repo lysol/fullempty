2cd8d5647d115728ab3c1efcaa11fc7ed06c6fe3
Derek Arnold <derek@derekarnold.net>
2019-04-23 08:35:41 -0500

    enable executable content scripts and add dummy weather file

---
+++ content/weather.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "<pre>"
+echo "This would be the weather if there was weather to put here."
+echo "</pre>"
--- src/index.sh
+++ src/index.sh
@@ -6,26 +6,46 @@ declare -A templatevars
 templatevars['year']=$(date +%Y)
 templatevars['date']=$(date)
 
-for k in `jq -r 'to_entries[] | .key' <content/default.json`; do
-    templatevars[${k}]=$(jq -r ".${k}" <content/default.json)
+defaultjson=$(< content/default.json)
+for k in `echo "$defaultjson" | jq -r 'to_entries[] | .key'`; do
+    templatevars[${k}]=$(echo $defaultjson | jq -r ".${k}")
 done
 
 # here we iterate over every file, copy the values from $templatevars,
 # and include anything from the json file
 for n in `ls -1 content/*.json | grep -v default.json | sort -V`; do
     echo "Processing $n"
+
+    # initialize document associative array prefilled with tempaltevars' values
     declare -A docvars
     for k in "${!templatevars[@]}"; do docvars[$k]="${templatevars[$k]}"; done
-    # (probably an easier way to do this, but)
-    for k in `jq -r 'to_entries[] | .key' <$n`; do
-        val=$(cat $n | jq -r ".${k}")
+
+    # (probably an easier way to do this, but) set the values from the file
+    docjson=$(<$n)
+    for k in `echo "$docjson" | jq -r 'to_entries[] | .key'`; do
+        val=$(echo "$docjson" | jq -r ".${k}")
         docvars[$k]=$val
     done
     # "template" is a special value that is the location of the base template to include
     output=$(cat ${docvars['template']})
+
     # "content" is more special, it's a filename that we cat the contents of to get the
     # actual value. might make it this more explicit rather than implicit later
-    docvars[content]=$(cat ${docvars[content]})
+    contentPath=${docvars[content]}
+
+    if [ -z "$contentPath" ]; then
+        echo "No content property in $n. Skipping."
+        continue
+    fi
+
+    if [[ -x "$contentPath" ]]; then
+        # file is executable, execute it to get our content
+        docvars[content]=$($contentPath)
+    else
+        # otherwise just redirect contents
+        docvars[content]=$(< "$contentPath")
+    fi
+
     for i in "${!docvars[@]}"
     do
         # hopefully this is enough to handle any ;s in the value
@@ -34,6 +54,7 @@ for n in `ls -1 content/*.json | grep -v default.json | sort -V`; do
         regex=$(echo "s;<% $i %>;$baseregexval;g")
         output=$(echo $output | sed -e "$regex")
     done
+
     outfn="$BUILD_DIR/${docvars[filename]}"
     echo $output>$outfn
     touch -d "${docvars[date]}" $outfn
