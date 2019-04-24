967ca144d9553869399b209de14b494fd27f5e66
Derek Arnold <derek@derekarnold.net>
2019-04-23 22:03:59 -0500

    remove test post

--- content/currentWeather.post.sh
+++
@@ -1,3 +0,0 @@
-docvars[title]=Weather
-docvars[content]=content/weather.sh
-docvars[filename]=weather.html
--- content/weather.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "<pre>"
-echo "This would be the weather if there was weather to put here."
-echo "</pre>"
