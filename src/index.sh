ce68847ec0e690bc186c6f38fa4edb379dd312e9
Derek Arnold <derek@derekarnold.net>
2019-04-25 08:43:17 -0500

    Perform template expansion in all variables, harden all values
    Previously accidental expansions could occur, like shell expansions in /
    from css comments lol
    
    Also, now you can use template variables ~everywhere~

--- src/index.sh
+++ src/index.sh
@@ -16,10 +16,12 @@ function replace_tag {
     local output="$3"
 
     # hopefully this is enough to handle any ;s in the value
-    local baseregexval=$(echo $val | sed -e 's/;/\\;/g')
+    # also replaces newlines with the escaped equiv so sed can replace it properly
+    local baseregexval=$(echo "$val" | sed -e 's/;/\\;/g' | sed ':a;N;$!ba;s/\n/\\n/g')
     # replace the template tags
+    # includes those escaped newlines in $baseregexval
     local regex=$(echo "s;<% $key %>;$baseregexval;g")
-    last_replace=$(echo $output | sed -e "$regex")
+    last_replace=$(echo "$output" | sed -e "$regex")
 }
 
 # here we iterate over every file, copy the values from $templatevars,
@@ -37,9 +39,9 @@ for n in $workfiles; do
     for k in "${!templatevars[@]}"; do docvars[$k]="${templatevars[$k]}"; done
 
     # source file to set variables
-    . $n
-    if [[ "${docvars[type]}" == "post" ]]; then
-        echo "$(date -d "${docvars[date]}" "+%s")${tab}${docvars[filename]}${tab}${docvars[title]}">>"${POST_INDEX}"
+    . "${n}"
+    if [[ "${docvars[type]}" == 'post' ]]; then
+        echo "$(date -d "${docvars[date]}" '+%s')${tab}${docvars[filename]}${tab}${docvars[title]}">>"${POST_INDEX}"
     fi
 done
 
@@ -48,64 +50,64 @@ IFS=$'\n'
 for line in $(cat "${POST_INDEX}" | sort -n); do
     IFS=$'\t' vars=(${line})
     # abuse the previous IFS little to put a newline in lol
-    posttext="${posttext}${IFS}<li>$(date -d @"${vars[0]}" "+%Y-%m-%d") <a href=\"${vars[1]}\">${vars[2]}</a></li>"
+    posttext="${posttext}${IFS}<li>$(date -d @"${vars[0]}" '+%Y-%m-%d') <a href=\"${vars[1]}\">${vars[2]}</a></li>"
 done
 unset IFS
 
 # set the post list in the base template so it can be used later
-templatevars['postitems']=${posttext}
+templatevars[postitems]=${posttext}
 
 echo Second pass
-echo $workfiles
 for n in $workfiles; do
-    echo "Working on $n (again)"
+    echo "Working on ${n} (again)"
     # initialize document associative array prefilled with templatevars' values
     declare -A docvars=()
     for k in "${!templatevars[@]}"; do docvars[$k]="${templatevars[$k]}"; done
 
     # source file to set variables
-    . $n
+    . "${n}"
 
     # "template" is a special value that is the location of the base template to include
-    output=$(cat ${docvars['template']})
+    output=$(< "${docvars['template']}")
 
     # "content" is more special, it's a filename that we cat the contents of to get the
     # actual value. might make it this more explicit rather than implicit later
-    contentPath=${docvars[content]}
+    contentPath="${docvars[content]}"
 
-    if [ -z "$contentPath" ]; then
-        echo "No content property in $n. Skipping."
+    if [ -z "${contentPath}" ]; then
+        echo "No content property in ${n}. Skipping."
         continue
     fi
 
     # considering doing this check for any value honestly
-    if [[ -x "$contentPath" ]]; then
+    if [[ -x "${contentPath}" ]]; then
         # file is executable, execute it to get our content
-        docvars[content]=$($contentPath)
+        docvars[content]="$($contentPath)"
     else
         # otherwise just redirect contents
-        docvars[content]=$(< "$contentPath")
+        docvars[content]="$(< "$contentPath")"
     fi
 
     for i in "${!docvars[@]}"
     do
-        # the content value is "special"
-        # we do replacements on this as well.
-        # possibly we could do it for every value but that
-        # might get weird
-        if [[ "$i" != "content" ]]; then
-            replace_tag "$i" "${docvars[$i]}" "${docvars[content]}"
-            docvars[content]="$last_replace"
-        fi
-        replace_tag "$i" "${docvars[$i]}" "$output"
-        output="$last_replace"
+        # this might be bad but we also go through each existing
+        # docvar and do replacements there too
+        for j in "${!docvars[@]}"
+        do
+            if [[ "${i}" != "${j}" ]]; then
+                replace_tag "${i}" "${docvars[${i}]}" "${docvars[${j}]}"
+                docvars["${j}"]="${last_replace}"
+            fi
+        done
+        replace_tag "${i}" "${docvars[$i]}" "${output}"
+        output="${last_replace}"
     done
 
     outfn="${BUILD_DIR}${docvars[filename]}"
     # create any intermediate directories
     mkdir -p $(dirname $outfn)
-    echo $output>$outfn
-    touch -d "${docvars[date]}" $outfn
+    echo "${output}">"${outfn}"
+    touch -d "${docvars[date]}" "${outfn}"
 done
 
 # finally, copy assets
