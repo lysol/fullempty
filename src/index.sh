146c330ddd73294535298c19f0dded6450d96bfd
Derek Arnold <derek@derekarnold.net>
2019-04-26 15:40:37 -0500

    move functions into their own files

--- src/index.sh
+++ src/index.sh
@@ -11,20 +11,9 @@ POST_INDEX="${BUILD_DIR}${POST_INDEX}"
 # make the build directory if it's not there yet :)
 mkdir -p $(dirname "${POST_INDEX}")
 
-last_replace=""
-function replace_tag {
-    local key="$1"
-    local val="$2"
-    local output="$3"
-
-    # hopefully this is enough to handle any ;s in the value
-    # also replaces newlines with the escaped equiv so sed can replace it properly
-    local baseregexval=$(echo "$val" | sed -e 's/\&/\\&/g' | sed -e 's/;/\\;/g' | sed ':a;N;$!ba;s/\n/\\n/g')
-    # replace the template tags
-    # includes those escaped newlines in $baseregexval
-    local regex=$(echo "s;<% $key %>;$baseregexval;g")
-    last_replace=$(echo "$output" | sed -e "$regex")
-}
+for n in $(ls -1 src/*.func.sh); do
+    . "${n}"
+done
 
 # here we iterate over every file, copy the values from $templatevars,
 # and include anything from the json file
