f3c6fdd9458b84391458103e248748c68c2eefc0
Derek Arnold <derek@derekarnold.net>
2019-04-27 10:10:33 -0500

    add cmd tag for running bash (BE CAREFUL)

--- src/replace_tags.func.sh
+++ src/replace_tags.func.sh
@@ -4,31 +4,77 @@ function replace_tags {
     local newoutput=""
 
     local current_token=''
+    local tag_char='%'
+    local cmd_char='$'
+    local open_tag='<'
+    local close_tag='>'
+    local new_line=$'\n'
     # text
     local BODY=1
     # starting cmd
-    local CMD=2
-    local IN_CMD=3
+    local TAG=2
+    local IN_TAG=3
+    local CMD=4
+    local IN_CMD=5
     local current_state="${BODY}"
 
     while IFS='' read -n1 c; do
         if [[ "${c}" == "" ]]; then
-            c=$'\n'
+            c="${new_line}"
         fi
         case "${current_state}" in
             "${BODY}")
                 case "${c}" in
-                    "<")
+                    "${open_tag}")
                         newoutput="${newoutput}${current_token}"
-                        current_token="<"
+                        current_token="${open_tag}"
                         ;;
-                    "%")
-                        if [[ "${current_token}" == "<" ]]; then
-                            current_state="${CMD}"
+                    "${tag_char}")
+                        if [[ "${current_token}" == "${open_tag}" ]]; then
+                            current_state="${TAG}"
                             current_token=""
                         else
                             current_token="${current_token}${c}"
                         fi
+                        ;;
+                    *)
+                        current_token="${current_token}${c}"
+                        ;;
+                esac
+                ;;
+            "${TAG}")
+                case "${c}" in
+                    " ")
+                        # eat it
+                        ;;
+                    "${tag_char}")
+                        # tag ending
+                        current_token="${c}"
+                        ;;
+                    "${close_tag}")
+                        # tag ending
+                        if [[ "${current_token}" == "${tag_char}" ]]; then
+                            current_state="${BODY}"
+                            current_token=""
+                        fi
+                        ;;
+                    *)
+                        current_state="${IN_TAG}"
+                        current_token="${c}"
+                        ;;
+                esac
+                ;;
+            "${IN_TAG}")
+                case "${c}" in
+                    " ")
+                        # ending IN_TOKEN
+                        for key in $keys; do
+                            if [[ "${current_token}" == "${key}" ]]; then
+                                val="${vars[${key}]}"
+                                newoutput="${newoutput}${val}"
+                            fi
+                        done
+                        current_state="${TAG}"
                         ;;
                     *)
                         current_token="${current_token}${c}"
@@ -40,14 +86,16 @@ function replace_tags {
                     " ")
                         # eat it
                         ;;
-                    "%")
+                    "${cmd_char}")
                         # cmd ending
-                        current_token="%"
+                        current_token="${c}"
                         ;;
-                    ">")
+                    "${close_tag}")
                         # cmd ending
-                        if [[ "${current_token}" == "%" ]]; then
+                        if [[ "${current_token}" == "${cmd_char}" ]]; then
                             current_state="${BODY}"
+                            val=$($current_token)
+                            newoutput="${newoutput}${val}"
                             current_token=""
                         fi
                         ;;
@@ -59,14 +107,9 @@ function replace_tags {
                 ;;
             "${IN_CMD}")
                 case "${c}" in
-                    " ")
-                        # ending IN_TOKEN
-                        for key in $keys; do
-                            if [[ "${current_token}" == "${key}" ]]; then
-                                val="${vars[${key}]}"
-                                newoutput="${newoutput}${val}"
-                            fi
-                        done
+                    "${cmd_char}")
+                        # ending IN_CMD
+                        # execute code
                         current_state="${CMD}"
                         ;;
                     *)
