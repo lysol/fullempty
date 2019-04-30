3b22c80a70cc55ac6ec7878ef39bdc19a9456ba4
Derek Arnold <derek@derekarnold.net>
2019-04-30 10:11:45 -0500

    move files into subdir

--- content/static-site.post.sh
+++ content/static-site.post.sh
@@ -1,6 +1,6 @@
 docvars[title]='A Pure Bash 4 Static Site Generator'
 docvars[template]=content/post.html
-docvars[content]=content/static-site.html
-docvars[filename]=static-site.html
+docvars[content]=content/posts/static-site.html
+docvars[filename]=posts/static-site.html
 docvars[date]='Thu Apr 25 08:54:13 CDT 2019'
 docvars[type]=post
--- content/youtube-dl.post.sh
+++ content/youtube-dl.post.sh
@@ -1,6 +1,6 @@
 docvars[title]='Archiving videos comprehensively with youtube-dl'
 docvars[template]=content/post.html
-docvars[content]=content/youtube-dl.html
-docvars[filename]=youtube-dl.html
+docvars[content]=content/posts/youtube-dl.html
+docvars[filename]=posts/youtube-dl.html
 docvars[date]='Tue Apr 23 21:46:48 CDT 2019'
 docvars[type]=post
