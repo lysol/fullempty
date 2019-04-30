17192f8376a05ded22a7d969cb049daf74c8e0f7
Derek Arnold <derek@derekarnold.net>
2019-04-30 10:52:52 -0500

    add python game dev post

---
+++ content/python-game-development.post.sh
@@ -0,0 +1,6 @@
+docvars[title]='A Few Thoughts on hobbyist Python Game Development'
+docvars[template]=content/post.html
+docvars[content]=content/posts/python-game-development.html
+docvars[filename]=posts/python-game-development.html
+docvars[date]='Tue Apr 30 18:12:20 CDT 2019'
+docvars[type]=post
