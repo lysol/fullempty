1bb3a4c6364827e07de11bcb7d887e9311cff1b4
Derek Arnold <derek@derekarnold.net>
2019-05-03 21:22:25 -0500

    sync time on all items

--- src/posttime.sh
+++ src/posttime.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 regex="s/.\+='\([^']\+\)'/\1/"
-for fn in $(IFS=$'\n' ls -1 content/*.post.sh); do
+for fn in $(IFS=$'\n' ls -1 content/*.sh); do
     ts=$(grep '\[date\]' "${fn}" | sed -e "${regex}")
-    touch -d "${ts}" "${fn}"
+    if [ "${ts}" != "" ]; then
+        echo "Setting timestamp '${ts}' on ${fn}"
+        touch -d "${ts}" "${fn}"
+    fi
 done
