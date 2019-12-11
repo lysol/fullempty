fc0078b5744561b122cc03f37ea0daaf2c36816f
Derek Arnold <derek@derekarnold.net>
2019-12-11 10:00:19 -0600

    add op25 post

---
+++ content/op25.post.sh
@@ -0,0 +1,7 @@
+docvars[title]='Building an OP25 setup for decoding P25 Digital Radio with a Raspberry Pi'
+docvars[description]='Listening to the cops on trunked digital radio with less than $100 of stuff.'
+docvars[template]=content/post.html
+docvars[content]=content/posts/op25.html
+docvars[filename]=posts/op25.html
+docvars[date]='Fri Jul  5 19:40:16 CDT 2019'
+docvars[type]=post
