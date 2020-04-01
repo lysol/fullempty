14ef4c0b65b97ba38ad54b232af5bc2099c91a27
Derek Arnold <derek@derekarnold.net>
2020-04-01 01:32:27 -0500

    add exeltel post

---
+++ content/exeltel2.post.sh
@@ -0,0 +1,7 @@
+docvars[title]='Breaking down the Exeltel II'
+docvars[description]='The US Minitel terminal.'
+docvars[template]=content/post.html
+docvars[content]=content/posts/exeltel2.html
+docvars[filename]=posts/exeltel2.html
+docvars[date]='Wed Dec 11 20:57:00 CST 2019'
+docvars[type]=post
