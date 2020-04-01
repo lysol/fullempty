1f98a2bb476fda4c970a477be68873047c7df8b2
Derek Arnold <derek@derekarnold.net>
2020-04-01 01:31:35 -0500

    add eprom dumping post

---
+++ content/eprom-dumping.post.sh
@@ -0,0 +1,7 @@
+docvars[title]='EPROM Dumping'
+docvars[description]='Taking a newly-built ROM programmer for a spin.'
+docvars[template]=content/post.html
+docvars[content]=content/posts/eprom-dumping.html
+docvars[filename]=posts/eprom-dumping.html
+docvars[date]='Wed Apr  1 01:13:57 CDT 2020'
+docvars[type]=post
