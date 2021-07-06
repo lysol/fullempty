3b4ac74f13e007a95b7c5e8036040db43f928c2b
Derek Arnold <derek@derekarnold.net>
2021-07-05 22:04:55 -0500

    add repair log post

---
+++ content/repair-log.post.sh
@@ -0,0 +1,7 @@
+docvars[title]='Repair Log 2021/7'
+docvars[description]='Notes from the last week'
+docvars[template]=content/post.html
+docvars[content]=content/posts/repair-log.html
+docvars[filename]=posts/repair-log.html
+docvars[date]='Mon Jul  5 19:43:24 CDT 2021'
+docvars[type]=post
