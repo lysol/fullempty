8413ef583ae05efc8ee790e2b971f5c8820f3d7a
Derek Arnold <derek@derekarnold.net>
2022-08-06 22:02:50 -0500

    add siemens 100a post

---
+++ content/t100.post.sh
@@ -0,0 +1,7 @@
+docvars[title]='Reviving a Siemens 100A Teleprinter'
+docvars[description]='How to get industrial deafness from checking your email'
+docvars[template]=content/post.html
+docvars[content]=content/posts/siemens_100a.html
+docvars[filename]=posts/siemens_100a.html
+docvars[date]='Sat Aug  6 21:40:00 CDT 2022'
+docvars[type]=post
