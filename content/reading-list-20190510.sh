f4d7ebb79918fbe6c9af08fde07e8317c69a51cc
Derek Arnold <derek@derekarnold.net>
2022-08-06 22:10:46 -0500

    drop reading list, I never updated it

--- content/reading-list-20190510.sh
+++
@@ -1,2 +0,0 @@
-docvars[date]='Fri May 10 09:39:45 EDT 2019'
-docvars[description]='<p><a href="https://www.youtube.com/watch?v=sDfIkXf3uzA">Interview with Samia Halaby</a> on her use of the Amiga in her art for the last three decades</p>'
--- content/zz_reading-list.rss.sh
+++
@@ -1,4 +0,0 @@
-docvars[content]=content/reading-list.rss
-docvars[filename]=reading-list.rss
-docvars[rssdate]=$(date -R)
-docvars[template]=''
